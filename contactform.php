122809c00edd46ca23ac2354fb523effdd78e331
dillondipetrillo <dillonrdipetrillo@gmail.com>
2020-10-20 19:03:57 -0400

    Update on contact section

--- contactform.php
+++ contactform.php
@@ -1,12 +1,33 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['mail'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+$message_sent = false;
+if(isset($_POST['email']) && $_POST['email'] != ''){
 
-    
+    if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+
+        //submit the form
+        $userName = $_POST['name'];
+        $userEmail = $_POST['email'];
+        $messageSubject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        $to = "dillonrdipetrillo@gmail.com";
+        $body = "";
+
+        $body .= "From: ".$userName. "\r\n";
+        $body .= "Email: ".$userEmail. "\r\n";
+        $body .= "Message: ".message. "\r\n";
+
+        mail($to,$messageSubject,$body);
+
+        message_sent = true;
+
+    } else {
+
+        $invalid_class_name = "form-invalid";
+
+    }
+
 }
 
 ?>
